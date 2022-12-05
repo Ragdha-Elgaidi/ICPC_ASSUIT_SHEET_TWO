850e766450f5777c5181cdf22fdb1ed93af5a436
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-05 05:15:39 +0200

    Create B. Even Numbers.cpp

---
+++ B. Even Numbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+
+	cin >> N;
+	if (N == 1)
+	{
+		cout << -1 << endl;
+    }
+	for (int i = 1; i <= N; i++)
+		if (i % 2 == 0)
+		{
+			cout << i << endl;
+		}
+		
+	return 0;
+}
