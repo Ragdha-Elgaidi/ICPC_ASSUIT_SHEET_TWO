9bf2051a1c644700cb2c22cc8693f42d3186a42d
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-07 05:04:36 +0200

    Create U. Some Sums.cpp

---
+++ U. Some Sums.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, b, total = 0;
+
+	cin >> n >> a >> b;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		int tmp = i;	
+		int digits_sum = 0;
+
+		while (tmp)
+		{
+			digits_sum += tmp % 10;
+			tmp /= 10;
+		}
+
+		if (a <= digits_sum && digits_sum <= b)
+			total += i;
+	}
+	cout << total << "\n";
+
+	return 0;
+}
