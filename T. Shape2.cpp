ea8cfdbeb9a067976883db8a33d7e8ec1a0a3556
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-07 04:39:23 +0200

    Create T. Shape2.cpp

---
+++ T. Shape2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{ 
+	int N;
+
+	cin >> N;
+	int row = 1;
+	while (row <= N)
+	{
+		int stars_count = 1;
+
+		/*
+		Spaces are : N - rows
+	    Starts are : 2 * row - 1
+		*/
+		while (stars_count <= N - row)
+		{
+			cout << ' ';
+			++stars_count;
+		}
+
+		stars_count = 1;
+		while (stars_count <= 2 * row - 1)
+		{
+			cout << '*';
+			++stars_count;
+		}
+		cout<<endl;
+		++row;
+	}
+
+	return 0;
+}
